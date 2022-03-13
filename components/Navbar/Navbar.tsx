8b8431fe1d5f7c91534f29d1fdc6c5ab8d3a8a69
khethelogp <khethelogp@gmail.com>
2022-03-13 23:58:40 +0200

    navbar styling

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,46 +1,49 @@
-import logo from '../../public/favicon.png';
-import { AppBar, Badge, IconButton, Toolbar, Typography } from '@mui/material'; 
+import logo from "../../public/favicon.png";
+import { AppBar, Badge, IconButton, Toolbar, Typography } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
-import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Image from "next/image";
+import { useRouter } from "next/router";
 import useStyles from "./styles";
 
-const Navbar = () => { 
+const Navbar = () => {
   const location = useRouter();
   const classes = useStyles();
 
   return (
     <>
-        <AppBar position='fixed' className={classes.appBar} color='inherit'>
-            <Toolbar>
-                <Typography
-                    variant='h6'
-                    className={classes.title}
-                    color='inherit' 
-                >
-                    <Image src={logo} alt='commerce.js' height={25} width={25} className={classes.image}/>
-                    e_Store
-                </Typography>
-                {/* <div style={{flexGrow: 1}} /> */}
-                <div className={classes.grow} />
-                {location.pathname === '/' && (
-                    <div>
-                        <IconButton
-                            // component={Link}
-                            // href='/cart'
-                            aria-label='Show cart items'
-                            color='inherit'
-                        >
-                            <Badge badgeContent='0' color='secondary'>
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
-                )}
-            </Toolbar>
-        </AppBar>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <Image
+              src={logo}
+              alt="commerce.js"
+              height={25}
+              width={25}
+              className={classes.image}
+            />
+            {"  "}
+            e_Store
+          </Typography>
+          {/* <div style={{flexGrow: 1}} /> */}
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            <div>
+              <IconButton
+                // component={Link}
+                // href='/cart'
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent="2" color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
