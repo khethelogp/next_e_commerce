7e107437f1c6b66cbd330ea9358841e889884ef5
khethelogp <khethelogp@gmail.com>
2022-03-13 22:27:07 +0200

    navbar

---
+++ components/Navbar/styles.ts
@@ -0,0 +1,27 @@
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const drawerWidth = 0;
+
+export default makeStyles((theme: Theme) => ({
+  appBar: {
+    boxShadow: "none",
+    borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+    [theme.breakpoints?.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  title: {
+    flexGrow: 1,
+    alignItems: "center",
+    display: "flex",
+    textDecoration: "none",
+  },
+  image: {
+    marginRight: "10px",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}));
