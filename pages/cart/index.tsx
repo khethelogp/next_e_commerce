cad3b16e3fec0550e0c79b153e0738a4f15f8fc0
khethelogp <khethelogp@gmail.com>
2022-04-11 00:24:38 +0200

    cart styles

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,62 +1,83 @@
-import React, { FC } from "react";
-import { Cart, IProduct } from "../../types/types";
-import useStyles from "./styles";
+import React from "react";
+import { Cart } from "../../types/types";
 import { Typography, Grid, Button, Container } from "@mui/material";
 import Link from "next/link";
 import theme from "../../src/theme";
+import { CartItem } from "../../components";
+import { useCommerce } from "../../context/CommerceContext";
 
-type Props = {
-  cart: Cart;
-  handleEmptyCart: Function;
-};
+// type Props = {
+//   cart: Cart;
+// };
 
-const Cart = ({ cart }: Props) => {
-  const classes = useStyles();
+const Cart = ({}) => {
+  const { cart } = useCommerce();
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
       You have no items in your shopping cart
       <Link href="/">
-        <a className={classes.link}> start addinf some items!</a>
+        {/* <a style={{ textDecoration: "none" }}> start adding some items!</a> */}
+        <a> start adding some items!</a>
       </Link>
     </Typography>
   );
 
+  const handleUpdateCartQty = () => {};
+  const handleRemoveFromCartQty = () => {};
+
   const FilledCart = () => (
     <>
-      {cart?.line_items.map((item) => (
-        <li>{item.name}</li>
-      ))}
-
-      {/* <Grid container spacing={3}>
-        {cart.line_items.map((item: IProduct) => (
+      <Grid container spacing={3}>
+        {cart?.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCartQty}/> 
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCartQty}
+            />
           </Grid>
         ))}
-      </Grid> */}
+      </Grid>
 
-      <div className={classes.cardDetails}>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "10%",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
         <Typography variant="h4">
-          Subtotal: {cart.subtotal.formatted_with_symbol}
+          Subtotal: {cart?.subtotal.formatted_with_symbol}
         </Typography>
         <div>
           <Button
-            className={classes.emptyButton}
             size="large"
             type="button"
             variant="contained"
             color="secondary"
+            sx={{
+              minWidth: "150px",
+              [theme.breakpoints?.down("xs")]: {
+                marginBottom: "5px",
+              },
+              [theme.breakpoints?.up("xs")]: {
+                marginRight: "20px",
+              },
+            }}
             // onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
           <Button
-            className={classes.checkoutButton}
             size="large"
             type="button"
             variant="contained"
             color="primary"
+            sx={{
+              minWidth: "150px",
+            }}
           >
             <Link href="checkout">Checkout</Link>
           </Button>
@@ -69,8 +90,8 @@ const Cart = ({ cart }: Props) => {
 
   return (
     <Container>
-      <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3" gutterBottom>
+      <div />
+      <Typography variant="h3" gutterBottom sx={{ marginTop: "5%" }}>
         Your Shopping Cart
       </Typography>
       {!cart?.line_items.length ? <EmptyCart /> : <FilledCart />}
